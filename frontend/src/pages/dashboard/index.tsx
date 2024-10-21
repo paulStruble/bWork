245700a40e2370514ec12af0faae65d2ee2d3630
Paul Struble <pjstruble1@gmail.com>
2024-10-21 07:35:49 -0700

    dashboard (update)

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -10,57 +10,106 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import HotBuildingsTable, { HotBuildingCount } from "./widgets/hotBuildings";
+import { BuildingRequestCount } from "./widgets/map/requestMap";
+import RecentCounter from "./widgets/recentCounter";
+import RequestMapContainer from "./widgets/map/requestMapContainer";
+import MapContainer from "./widgets/map/requestMapContainer";
 
 
 const apiEntry = process.env.NEXT_PUBLIC_API_ENTRYPOINT;
 
-function formatHotBuildingsData(hotBuildingsData) {
+function formatHotBuildingsData(hotBuildingsData: HotBuildingCount[]) {
   return hotBuildingsData.map((building) => {
     return {
       building: building.building,
-      count: parseInt(building.count, 10), // Convert count to integer
+      count: building.count, // Convert count to integer
       average_priority: parseFloat(building.average_priority).toFixed(1) // Truncate to 1 decimal
     };
   });
 }
 
 export const getStaticProps = (async () => {
-  const [reqRequestBarChartData, reqItemLineChartData, reqHotBuildingsDataPre] = await Promise.all([
+  const [
+    reqRequestBarChartData,
+    reqItemLineChartData,
+    reqHotBuildingsDataPre,
+    reqBuildingRequestCounts,
+    reqRecentRequestCount,
+    reqRecentOrderCount
+    ] = await Promise.all([
     fetch(`${apiEntry}/requestChart`),
     fetch(`${apiEntry}/dailyItemCounts`),
-    fetch(`${apiEntry}/hotBuildingCounts`)
+    fetch(`${apiEntry}/hotBuildingCounts`),
+    fetch(`${apiEntry}/buildingRequestCounts`),
+    fetch(`${apiEntry}/recentRequestCount`),
+    fetch(`${apiEntry}/recentOrderCount`),
   ]);
 
-  const [requestBarChartData, itemLineChartData, hotBuildingsDataPre] = await Promise.all([
+  const [
+    requestBarChartData, 
+    itemLineChartData, 
+    hotBuildingsDataPre, 
+    buildingRequestCounts,
+    recentRequestCount,
+    recentOrderCount
+  ] = await Promise.all([
     reqRequestBarChartData.json(),
     reqItemLineChartData.json(),
     reqHotBuildingsDataPre.json(),
+    reqBuildingRequestCounts.json(),
+    reqRecentRequestCount.json(),
+    reqRecentOrderCount.json(),
   ]);
 
   const hotBuildingsData = formatHotBuildingsData(hotBuildingsDataPre);
 
-  return { props: { requestBarChartData, itemLineChartData, hotBuildingsData } };
+  return { props: { 
+    requestBarChartData, 
+    itemLineChartData, 
+    hotBuildingsData, 
+    buildingRequestCounts,
+    recentRequestCount,
+    recentOrderCount
+  } };
 }) satisfies GetStaticProps<{
   requestBarChartData: MonthlyRequestsCount[],
   itemLineChartData: DailyItemCount[],
   hotBuildingsData: HotBuildingCount[],
+  buildingRequestCounts: BuildingRequestCount[],
+  recentRequestCount: number,
+  recentOrderCount: number,
 }>
 
 export default function Dashboard({ 
   requestBarChartData,
   itemLineChartData,
-  hotBuildingsData
+  hotBuildingsData,
+  buildingRequestCounts,
+  recentRequestCount,
+  recentOrderCount,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <main className="min-h-screen flex flex-col items-center center-align justify-center">
-      <div className="w-fullmax-w-4xl">
-        <RequestBarChart chartData={requestBarChartData}/>
+    <main className="flex flex-row bg-red-500 h-screen w-screen">
+      <div className="flex flex-col justify-between bg-blue-500 h-full">
+        <div className="p-0">
+          <RecentCounter
+            requestCount={recentRequestCount}
+            orderCount={recentOrderCount}
+          />
+        </div>
+        <div className="bg-purple-400">
+          <HotBuildingsTable tableData={hotBuildingsData}/>
+        </div>
       </div>
-      <div className="w-ful max-w-4xl mt-4">
-        <RequestLineChart chartData={itemLineChartData} />
-      </div>
-      <div>
-        <HotBuildingsTable tableData={hotBuildingsData}/>
+      <div className="flex flex-col bg-green-500 h-full flex-grow">
+        <div className="block w-full">
+          <RequestLineChart
+          chartData={itemLineChartData}
+          />
+        </div>
+        <div className="bg-yellow-400 flex-grow">
+          <RequestMapContainer buildingRequestCounts={buildingRequestCounts}/>
+        </div>
       </div>
     </main>
   );
